d3e4dd6a39d3e8d92d4f77c3edf555b74963d3fd
Karsten Vermeulen <djkarstenv@gmail.com>
2017-12-10 16:02:27 +0000

    Small changes
    The camera received a few small tweaks, such as the way it passes data to the shaders. The Color and Client classes were removed as networking is a feature that will be worked on at a later stage and color will be encapsulated using GLM later on!

--- Handmade/Camera.cpp
+++ Handmade/Camera.cpp
@@ -1,13 +1,10 @@
 #include <gtc\matrix_transform.hpp>
 #include "Camera.h"
 #include "Game.h"
+#include "PipelineManager.h"
 #include "ScreenManager.h"
-#include "ShaderManager.h"
 
-//------------------------------------------------------------------------------------------------------
-//static member variable initializations
-//------------------------------------------------------------------------------------------------------
-GLuint Camera::s_viewUniformID = 0;
+glm::vec3 Camera::s_position;
 glm::mat4 Camera::s_viewMatrix;
 
 //------------------------------------------------------------------------------------------------------
@@ -20,48 +17,16 @@ void Camera::SetIdentity()
 
 }
 //------------------------------------------------------------------------------------------------------
-//static function that sends camera's view matrix data to the vertex shader
+//static function that sends all camera's main properties to shaders
 //------------------------------------------------------------------------------------------------------
-void Camera::ApplyMatrix()
+void Camera::SendToShader()
 {
 
-	TheShader::Instance()->SetUniformMatrix(s_viewUniformID, &s_viewMatrix[0][0]);
+	//send view matrix data to the vertex shader
+	ThePipeline::Instance()->SendUniformData("viewMatrix", s_viewMatrix);
 
-}
-//------------------------------------------------------------------------------------------------------
-//static setter function that assigns view matrix's shader uniform ID  
-//------------------------------------------------------------------------------------------------------
-void Camera::SetViewUniformID(std::string uniformID)
-{
-
-	s_viewUniformID = TheShader::Instance()->GetUniformID(uniformID.c_str());
-
-}
-//------------------------------------------------------------------------------------------------------
-//constructor that assigns all default values 
-//------------------------------------------------------------------------------------------------------
-Camera::Camera()
-{
-
-	m_freeFlow = false;
-	m_velocity = 0.0f;
-	m_sensitivity = 1.0f;
-
-	m_threshold.x = -0.90f;
-	m_threshold.y = 0.90f;
-
-	m_lookAt = glm::vec3(0.0f, 0.0f, -1.0f);
-	m_upVector = glm::vec3(0.0f, 1.0f, 0.0f);
-	m_viewDirection = glm::vec3(0.0f, 0.0f, -1.0f);
-
-}
-//------------------------------------------------------------------------------------------------------
-//getter-setter function that returns free flow movement flag reference variable for camera 
-//------------------------------------------------------------------------------------------------------
-bool& Camera::IsFreeFlow()
-{
-
-	return m_freeFlow;
+	//send camera position to fragment shader
+	ThePipeline::Instance()->SendUniformData("cameraPosition", s_position);
 
 }
 //------------------------------------------------------------------------------------------------------
@@ -70,22 +35,36 @@ bool& Camera::IsFreeFlow()
 glm::vec3& Camera::Position()
 {
 
-	return m_position;
+	return s_position;
 
 }
 //------------------------------------------------------------------------------------------------------
-//getter function that returns viewing direction vector of camera
+//constructor that assigns all default values 
 //------------------------------------------------------------------------------------------------------
-glm::vec3 Camera::GetViewDirection()
+Camera::Camera()
 {
 
-	return m_viewDirection;
+	m_isFreeFlow = false;
+	m_velocity = 0.07f;
+	m_sensitivity = 1.0f;
+	m_upVector.y = 1.0f;
+	m_threshold = glm::vec2(-0.90f, 0.90f);
+	m_lookAt = glm::vec3(0.0f, 0.0f, -1.0f);
+
+}
+//------------------------------------------------------------------------------------------------------
+//getter-setter function that returns free flow movement flag reference variable for camera 
+//------------------------------------------------------------------------------------------------------
+bool& Camera::IsFreeFlow()
+{
+
+	return m_isFreeFlow;
 
 }
 //------------------------------------------------------------------------------------------------------
 //setter function that assigns speed of camera
 //------------------------------------------------------------------------------------------------------
-void Camera::SetVelocity(float velocity)
+void Camera::SetVelocity(GLfloat velocity)
 {
 
 	m_velocity = velocity;
@@ -94,7 +73,7 @@ void Camera::SetVelocity(float velocity)
 //------------------------------------------------------------------------------------------------------
 //setter function that assigns camera rotation sensitivity
 //------------------------------------------------------------------------------------------------------
-void Camera::SetSensitivity(float sensitivity)
+void Camera::SetSensitivity(GLfloat sensitivity)
 {
 
 	m_sensitivity = sensitivity;
@@ -103,22 +82,19 @@ void Camera::SetSensitivity(float sensitivity)
 //------------------------------------------------------------------------------------------------------
 //setter function that assigns camera's min and max Y threshold value
 //------------------------------------------------------------------------------------------------------
-void Camera::SetThreshold(float min, float max)
+void Camera::SetThreshold(GLfloat min, GLfloat max)
 {
 
-	m_threshold.x = min;
-	m_threshold.y = max;
+	m_threshold = glm::vec2(min, max);
 
 }
 //------------------------------------------------------------------------------------------------------
 //setter function that assigns initial viewing direction of camera
 //------------------------------------------------------------------------------------------------------
-void Camera::SetLookAt(float x, float y, float z)
+void Camera::SetLookAt(GLfloat x, GLfloat y, GLfloat z)
 {
 
-	m_lookAt.x = x;
-	m_lookAt.y = y;
-	m_lookAt.z = z;
+	m_lookAt = glm::vec3(x, y, z);
 
 }
 //------------------------------------------------------------------------------------------------------
@@ -127,29 +103,37 @@ void Camera::SetLookAt(float x, float y, float z)
 void Camera::Update()
 {
 
-	glm::mat4 tempRotation;
+	//set camera to initially look forward in the negative Z-axis 
+	//this view will be transformed by the camera's rotation below
+	m_lookAt = glm::vec3(0.0f, 0.0f, -1.0f);
+
+	//create a total rotation transformation based on X and Y rotations
+	//we do a Y rotation FIRST and then a local X rotation thereafter
 	glm::mat4 totalRotation = m_rotationMatrixY * m_rotationMatrixX;
-	
+
 	//calculate the camera's view direction vector by transforming 
 	//the initial viewing target based on camera's total rotation
-	m_viewDirection = glm::vec3(totalRotation * glm::vec4(m_lookAt, 1.0f));
+	m_lookAt = glm::vec3(totalRotation * glm::vec4(m_lookAt, 1.0f));
 
-	//based on if camera is in freeflow or non-freeflow mode, set temp rotation to be used below
-	//this will either be the total rotation based on X and Y rotations or only the Y rotation
-	((m_freeFlow) ? tempRotation = m_rotationMatrixY * m_rotationMatrixX
-		          : tempRotation = m_rotationMatrixY);
+	//create a separate move rotation based on if camera is in freeflow mode or not
+	//this affects movement so that camera either moves freely or is bound to a Y plane 
+	glm::mat4 moveRotation = (m_isFreeFlow) ? totalRotation : m_rotationMatrixY;
+
+	//apply same rotation to move direction vector so that
+	//the camera moves correctly based on camera orientation
+	m_moveDirection = glm::vec3(moveRotation * glm::vec4(m_moveDirection, 1.0f));
 
 	//if the camera is set to move, calculate its position using the chosen rotation, 
 	//the direction the camera is moving in, the speed at which its moving and time
 	if (m_moveDirection != glm::vec3(0.0f))
 	{
-		
-		m_position += glm::vec3(tempRotation * glm::vec4(m_moveDirection, 1.0f)) * m_velocity *
-			          (float)TheGame::Instance()->GetElapsedTimeSeconds();
+
+		s_position += m_moveDirection * m_velocity *
+			          ((float)TheGame::Instance()->GetElapsedTime() / 1000);
 	}
 
 	//update camera's view matrix
-	s_viewMatrix = glm::lookAt(m_position, m_position + m_viewDirection, m_upVector);
+	s_viewMatrix = glm::lookAt(s_position, s_position + m_lookAt, m_upVector);
 
 }
 //------------------------------------------------------------------------------------------------------
@@ -222,19 +206,19 @@ void Camera::RotateX(short motionY)
 {
 
 	//generate a rotation angle based on mouse motion and rotation sensitivity
-	float angle = motionY * m_sensitivity;
+	GLfloat angle = motionY * m_sensitivity;
 
-	//calculate the dot product between the "look at" vector and the up vector. This will give 
-	//us a value to determine how far up or down the X axis the camera is tilting
-	double dot = glm::dot(m_viewDirection, m_upVector);
+	//calculate the dot product between the "look at" vector and the up vector. This will
+	//give us a value to determine how far up or down the X axis the camera is tilting
+	double dot = glm::dot(m_lookAt, m_upVector);
 
 	//if the camera is tilting upwards or downwards and only if it is between its allowed 
-	//threshold, accumulate rotation matrix based on rotation created around X axis
+	//threshold, then accumulate the current X axis rotation using the angle calculated
 	if ((motionY > 0 && dot >= m_threshold.x) || (motionY < 0 && dot <= m_threshold.y))
 	{
 		m_rotationMatrixX = glm::rotate(m_rotationMatrixX,
 						    glm::radians(angle),
-							glm::vec3(-1.0f, 0.0f, 0.0f));
+						    glm::vec3(-1.0f, 0.0f, 0.0f));
 	}
 
 }
@@ -245,9 +229,9 @@ void Camera::RotateY(short motionX)
 {
 
 	//generate a rotation angle based on mouse motion and rotation sensitivity
-	float angle = motionX * m_sensitivity;
+	GLfloat angle = motionX * m_sensitivity;
 
-	//accumulate rotation matrix based on rotation created around Y axis
+	//accumulate the current Y axis rotation using the angle calculated
 	m_rotationMatrixY = glm::rotate(m_rotationMatrixY,
 						glm::radians(angle),
 						glm::vec3(0.0f, -1.0f, 0.0f));
--- Handmade/Camera.h
+++ Handmade/Camera.h
@@ -6,7 +6,7 @@
   and anyone else wishing to learn C++ and OOP. Feel free to use, copy, break, update and do as
   you wish with this code - it is there for all!
 
-  UPDATED : January 2017
+  UPDATED : December 2017
 
   -----------------------------------------------------------------------------------------------
 
@@ -19,23 +19,24 @@
   
 - There are a few static variables and functions which were added to allow global access to the 
   camera's main properties, that belong to the camera. Of these is the view matrix, because we only
-  need one matrix to represent the camera view. The other variable is the ID that monitors the 
-  vertex shader's view matrix uniform, to which the view matrix is passed to. The static functions
-  allow for the view matrix to be reset to the identity and passed to the vertex shader. The view
-  matrix is transformed in the Update() routine, and no need to manually transform it, unlike how
-  the GameObject class deals with its model matrix.
+  need one matrix to represent the camera view. The other variable is the position that monitors the 
+  camera's location in the game world. The static functions allow for the view matrix to be reset to
+  the identity and passed to the shaders. The view matrix is transformed in the Update() routine, 
+  and no need to manually transform it, unlike how the GameObject class deals with its model matrix.
 
 - The member variables of the class define all the basic properties of the camera, such as position,
   speed, sensitivity, etc. The free flow variable allows setting the free flow mode on or off, which
   controls if the camera can roam freely in the game world, or if it is set to a specific horizontal
   Y plane respectively.
-  There are two main matrix variables, one to hold the X rotations and one to hold the Y rotations
+  
+- There are two main matrix variables, one to hold the X rotations and one to hold the Y rotations
   of the camera. If the camera is free flowing, the total rotations are used, ie X and Y combined,
   making sure the camera moves forwards / backwards / up / down, taking X rotations into account.
   Otherwise for a non-freeflow camera, only the Y rotations are used, meaning that no matter how 
   much the camera is rotated in X, the forwards / backwards / up / down movements remains on the 
   global horizontal Y plane.
-  The m_moveDirection variable determines which direction the camera moves in and the 
+  
+- The m_moveDirection variable determines which direction the camera moves in and the 
   m_viewDirection determines where the camera is looking. Both these properties are not always 
   going to be the same. There is also a m_threshold variable which determines the min and max
   allowance value that the camera can tilt on its X axis, so that it doesn't look upwards or 
@@ -75,12 +76,12 @@ class Camera
 public:
 
 	static void SetIdentity();
-	static void ApplyMatrix();
-	static void SetViewUniformID(std::string uniformID);
+	static void SendToShader();
+	static glm::vec3& Position();
 
 private:
 
-	static GLuint s_viewUniformID;
+	static glm::vec3 s_position;
 	static glm::mat4 s_viewMatrix;
 
 public:
@@ -90,20 +91,18 @@ public:
 public:
 
 	bool& IsFreeFlow();
-	glm::vec3& Position();
-	glm::vec3 GetViewDirection();
 
 public:
 
-	void SetVelocity(float velocity);
-	void SetSensitivity(float sensitivity);
-	void SetThreshold(float min, float max);
-	void SetLookAt(float x, float y, float z);
+	void SetVelocity(GLfloat velocity);
+	void SetSensitivity(GLfloat sensitivity);
+	void SetThreshold(GLfloat min, GLfloat max);
+	void SetLookAt(GLfloat x, GLfloat y, GLfloat z);
 
 public:
 
 	void Update();
-
+	
 public:
 
 	void Stop();
@@ -118,15 +117,14 @@ public:
 
 private:
 
-	bool m_freeFlow;
-	float m_velocity;
-	float m_sensitivity;
-
-	glm::vec2 m_threshold;
-	glm::vec3 m_position;
+	bool m_isFreeFlow;
+	
+	GLfloat m_velocity;
+	GLfloat m_sensitivity;
 
 	glm::vec3 m_lookAt;
 	glm::vec3 m_upVector;
+	glm::vec2 m_threshold;
 	glm::vec3 m_moveDirection;
 	glm::vec3 m_viewDirection;
 
