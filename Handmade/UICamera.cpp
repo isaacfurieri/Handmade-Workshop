b80cfe4522a7da35d3995333250554fb15a7034f
Karsten Vermeulen <djkarstenv@gmail.com>
2021-06-24 20:41:31 +0100

    Major engine refactor - removed all cameras

--- Handmade/UICamera.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtc\matrix_transform.hpp>
-#include "Shader.h"
-#include "UICamera.h"
-
-//======================================================================================================
-void UICamera::Update(int deltaTime)
-{
-	m_viewMatrix = glm::lookAt(m_position, m_position + m_forward, m_up);
-}
