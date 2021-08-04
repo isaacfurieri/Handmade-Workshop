63a5a666a7264f93a87c11485b4dd6b669b23f12
Karsten Vermeulen <djkarstenv@gmail.com>
2021-08-04 20:21:10 +0100

    Added Capsule collider

---
+++ Handmade/Capsule.h
@@ -0,0 +1,27 @@
+#pragma once
+
+/*===================================================================#
+| 'Capsule' source files last updated on 4 August 2021               |
+#===================================================================*/
+
+#include "glad.h"
+#include <glm.hpp>
+#include "SphereCollider.h"
+//#include "LineSegment.h"
+
+class Capsule
+{
+
+public:
+
+	Capsule();
+	Capsule(const glm::vec3& vertex1, const glm::vec3& vertex2, GLfloat radius);
+
+	bool IsColliding(const SphereCollider& secondSphere) const;
+
+private:
+
+	//LineSegment m_linesegment;
+	GLfloat m_radius;
+
+};
