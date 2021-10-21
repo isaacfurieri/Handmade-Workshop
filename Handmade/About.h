5a5ee15d7f358bc28d5af54788405c28783aec3b
Karsten <djkarstenv@gmail.com>
2021-10-21 16:59:30 +0100

    Add class to manage 'About' dialog box

---
+++ Handmade/About.h
@@ -0,0 +1,21 @@
+#pragma once
+
+/*===================================================================#
+| 'About' source files last updated on 21 October 2021               |
+#===================================================================*/
+
+#include <afxdialogex.h>
+
+class About : public CDialogEx
+{
+
+public:
+
+	About();
+
+private:
+
+	virtual void DoDataExchange(CDataExchange* dataExchange);
+	DECLARE_MESSAGE_MAP()
+
+};
